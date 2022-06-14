b864669802055a530c64f91e0c19ed4fa5b5c81d
azoom-thai-doan-dat <thai.doan.dat@azoom.jp>
2022-06-14 22:26:18 +0900

    :+1: Demo firebase ui

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyDGuOuFxERwHWUPYF3yVpXZm4ptByexBB4",
+  authDomain: "fir-firebase-ui-87722.firebaseapp.com",
+  projectId: "fir-firebase-ui-87722",
+  storageBucket: "fir-firebase-ui-87722.appspot.com",
+  messagingSenderId: "739539204383",
+  appId: "1:739539204383:web:7af4672282cfcfa246182a",
+  measurementId: "G-W8DSVCQENL"
+};
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,49 @@
 <template>
-  <div>Hello world!</div>
+  <div>
+    <h2 v-if="user">Signed in user: {{ user }}</h2>
+    <div id="firebaseui-auth-container"></div>
+    <div id="loader">Loading...</div>
+  </div>
 </template>
 
 <script>
+import firebase from 'firebase/compat/app'
+import * as firebaseui from 'firebaseui'
+import firebaseConfig from '../firebaseConfig'
+import 'firebaseui/dist/firebaseui.css'
+firebase.initializeApp(firebaseConfig)
+const ui = new firebaseui.auth.AuthUI(firebase.auth())
+
 export default {
   name: 'IndexPage',
+  data() {
+    return {
+      user: '',
+      isSignedIn: false,
+    }
+  },
+  mounted() {
+    ui.start('#firebaseui-auth-container', {
+      signInFlow: 'popup',
+      // signInSuccessUrl: 'http://localhost:3001/protect',
+      signInOptions: [
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      ],
+      callbacks: {
+        signInSuccessWithAuthResult: (authResult) => {
+          this.user = authResult.user.displayName || 'new User'
+          console.log(authResult)
+          this.isSignedIn = true
+
+          return false
+        },
+        uiShown() {
+          document.getElementById('loader').style.display = 'none'
+        }
+      }
+    })
+  },
 }
 </script>
